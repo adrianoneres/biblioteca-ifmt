5cbc25b5a80f226499625232c5fb31723fd68f8e
Adriano Souza <ah.driano@gmail.com>
2023-10-11 14:50:54 -0400

    fix: record id

--- scripts.sql
+++ scripts.sql
@@ -5,4 +5,4 @@ CREATE TABLE livros (id UUID PRIMARY KEY, titulo VARCHAR(100) NOT NULL, autor VA
 INSERT INTO livros VALUES ('cff21df5-f959-4630-a3a1-f6b272951800', 'Dracula', 'Bram Stoker');
 INSERT INTO livros VALUES ('6b8328bc-2daf-4782-9350-87226ee5ee4e', 'Hamlet', 'Shakespeare');
 INSERT INTO livros VALUES ('5bb56b52-afbd-4d1d-9133-1b19175aed7d', 'Crime e Castigo', 'Fiódor Dostoiévski');
-INSERT INTO livros VALUES ('5bb56b52-afbd-4d1d-9133-1b19175aed7d', 'Anna Karênina', 'Liev Tolstói');
+INSERT INTO livros VALUES ('6c59ad1b-1b9d-47d4-a98c-b74410cc039a', 'Anna Karênina', 'Liev Tolstói');
